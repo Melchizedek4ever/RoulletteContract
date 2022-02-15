f5d8dc0ef94b6f254e567d8af51f50413b5cd245
Ezra Kewa <88113467+Melchizedek4ever@users.noreply.github.com>
2022-02-15 21:36:30 +0100

    initial commit

---
+++ scripts/2.run.sh
@@ -0,0 +1,24 @@
+
+#!/usr/bin/env bash
+
+# exit on first error after this point to avoid redeploying with successful build
+set -e
+
+echo
+echo \$CONTRACT is $CONTRACT
+echo \$OWNER is $OWNER
+
+
+#Step 1: Create a Roulette Game by uncommenting ONLY the following line and then running the script
+near call $CONTRACT createGame --amount 10 --account_id $OWNER
+
+#Step 2: Join the Game by uncommenting ONLY the near call line below and then running the script. 
+#This needs the following arguments:
+#Pass in the _gameid number returned in the command line from calling createGame 
+#Pass in your guess as either true or false
+#Pass in an amount of money 
+#finally pass in your testnet account
+near call $CONTRACT joinGame '{"_gameId":3856165906, "_guess":false}' --amount 10 --account_id melchizedek.testnet
+
+#Step 3: call endGame by uncommenting ONLY the following line and then running the script
+near call $CONTRACT endGame
